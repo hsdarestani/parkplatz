19ab318992bfd10bd8a9c34bccc23432adc0bd5c
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-18 01:49:08 +0330

    feat: route profile vehicle and host experiences

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,51 +1,77 @@
 import 'package:go_router/go_router.dart';
 
+import '../features/account/presentation/profile_screen.dart';
+import '../features/account/presentation/vehicles_screen.dart';
 import '../features/booking/presentation/booking_screens.dart';
 import '../features/discovery/presentation/discovery_screen.dart';
+import '../features/host/presentation/host_dashboard_screen.dart';
+import '../features/host/presentation/host_listing_wizard.dart';
 import '../features/launch/launch_screen.dart';
 
 GoRouter createRouter() => GoRouter(
       routes: [
-        GoRoute(path: '/', builder: (c, s) => const LaunchScreen()),
-        GoRoute(path: '/discover', builder: (c, s) => const DiscoveryScreen()),
+        GoRoute(path: '/', builder: (context, state) => const LaunchScreen()),
+        GoRoute(
+          path: '/discover',
+          builder: (context, state) => const DiscoveryScreen(),
+        ),
         GoRoute(
           path: '/search',
-          builder: (c, s) => const DiscoveryScreen(results: true),
+          builder: (context, state) => const DiscoveryScreen(results: true),
         ),
         GoRoute(
           path: '/parking/:id',
-          builder: (c, s) =>
-              ParkingDetailScreen(id: s.pathParameters['id']!),
+          builder: (context, state) =>
+              ParkingDetailScreen(id: state.pathParameters['id']!),
         ),
         GoRoute(
           path: '/login',
-          builder: (c, s) => AuthScreen(
+          builder: (context, state) => AuthScreen(
             register: false,
-            returnTo: s.uri.queryParameters['returnTo'],
+            returnTo: state.uri.queryParameters['returnTo'],
           ),
         ),
         GoRoute(
           path: '/register',
-          builder: (c, s) => AuthScreen(
+          builder: (context, state) => AuthScreen(
             register: true,
-            returnTo: s.uri.queryParameters['returnTo'],
+            returnTo: state.uri.queryParameters['returnTo'],
           ),
         ),
         GoRoute(
           path: '/checkout/:id',
-          builder: (c, s) => CheckoutScreen(id: s.pathParameters['id']!),
+          builder: (context, state) =>
+              CheckoutScreen(id: state.pathParameters['id']!),
         ),
         GoRoute(
           path: '/booking/:id/confirmed',
-          builder: (c, s) => ConfirmationScreen(id: s.pathParameters['id']!),
+          builder: (context, state) =>
+              ConfirmationScreen(id: state.pathParameters['id']!),
         ),
         GoRoute(
           path: '/bookings',
-          builder: (c, s) => const BookingsScreen(),
+          builder: (context, state) => const BookingsScreen(),
         ),
         GoRoute(
           path: '/bookings/:id/pass',
-          builder: (c, s) => ParkingPassScreen(id: s.pathParameters['id']!),
+          builder: (context, state) =>
+              ParkingPassScreen(id: state.pathParameters['id']!),
+        ),
+        GoRoute(
+          path: '/profile',
+          builder: (context, state) => const ProfileScreen(),
+        ),
+        GoRoute(
+          path: '/vehicles',
+          builder: (context, state) => const VehiclesScreen(),
+        ),
+        GoRoute(
+          path: '/host',
+          builder: (context, state) => const HostDashboardScreen(),
+        ),
+        GoRoute(
+          path: '/host/new',
+          builder: (context, state) => const HostListingWizardScreen(),
         ),
       ],
     );
