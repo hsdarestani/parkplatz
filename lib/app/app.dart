7fb3016e920d411318b4814fdb487c13c1f9f912
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-18 00:30:21 +0330

    fix: create GoRouter lazily inside app state

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
 import '../config/brand_config.dart';
 import '../core/theme/app_theme.dart';
 import 'router.dart';
-class FreiraumApp extends StatelessWidget{const FreiraumApp({super.key});@override Widget build(BuildContext context)=>ProviderScope(child:MaterialApp.router(title:BrandConfig.name,theme:appTheme(),debugShowCheckedModeBanner:false,routerConfig:router));}
+
+class FreiraumApp extends StatefulWidget {
+  const FreiraumApp({super.key});
+
+  @override
+  State<FreiraumApp> createState() => _FreiraumAppState();
+}
+
+class _FreiraumAppState extends State<FreiraumApp> {
+  late final GoRouter _router = createRouter();
+
+  @override
+  void dispose() {
+    _router.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      child: MaterialApp.router(
+        title: BrandConfig.name,
+        theme: appTheme(),
+        debugShowCheckedModeBanner: false,
+        routerConfig: _router,
+      ),
+    );
+  }
+}
