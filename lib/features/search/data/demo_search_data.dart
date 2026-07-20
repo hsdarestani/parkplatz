89f3b478ec2d0a17c17008a06300e871746fc50b
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-20 10:58:35 +0330

    feat: add simple vehicle classes for search

--- lib/features/search/data/demo_search_data.dart
+++ lib/features/search/data/demo_search_data.dart
@@ -1,3 +1,44 @@
 import '../../../shared/models/models.dart';
-final demoDestinations=<Destination>[const Destination('messe','Messe Frankfurt','Westend',50.111,8.650),const Destination('hbf','Frankfurt Hauptbahnhof','Gutleutviertel',50.107,8.663),const Destination('uniklinik','Universitätsklinikum Frankfurt','Niederrad',50.096,8.659),const Destination('alteoper','Alte Oper','Innenstadt',50.116,8.672),const Destination('skyline','Skyline Plaza','Gallus',50.110,8.652),const Destination('flughafen','Flughafen Frankfurt Terminal 1','Flughafen',50.050,8.570),const Destination('roemer','Römerberg','Altstadt',50.110,8.682),const Destination('warte','Bockenheimer Warte','Bockenheim',50.120,8.653)];
-final demoVehicles=<Vehicle>[const Vehicle('golf','VW Golf','F-RM 204',1.49,1.79,4.28),const Vehicle('bmw3','BMW 3er','F-MT 310',1.44,1.83,4.71),const Vehicle('vito','Mercedes Vito','F-VI 900',1.91,1.93,5.14),const Vehicle('custom','Eigenes Fahrzeug hinzufügen','Demo',1.80,1.90,4.80)];
+
+const demoDestinations = <Destination>[
+  Destination('messe', 'Messe Frankfurt', 'Westend', 50.111, 8.650),
+  Destination(
+    'hbf',
+    'Frankfurt Hauptbahnhof',
+    'Gutleutviertel',
+    50.107,
+    8.663,
+  ),
+  Destination(
+    'uniklinik',
+    'Universitätsklinikum Frankfurt',
+    'Niederrad',
+    50.096,
+    8.659,
+  ),
+  Destination('alteoper', 'Alte Oper', 'Innenstadt', 50.116, 8.672),
+  Destination('skyline', 'Skyline Plaza', 'Gallus', 50.110, 8.652),
+  Destination(
+    'flughafen',
+    'Flughafen Frankfurt Terminal 1',
+    'Flughafen',
+    50.050,
+    8.570,
+  ),
+  Destination('roemer', 'Römerberg', 'Altstadt', 50.110, 8.682),
+  Destination(
+    'warte',
+    'Bockenheimer Warte',
+    'Bockenheim',
+    50.120,
+    8.653,
+  ),
+];
+
+const demoVehicles = <Vehicle>[
+  Vehicle('class-small', 'Kleinwagen', 'z. B. VW Polo', 1.50, 1.78, 4.10),
+  Vehicle('class-compact', 'Kompaktklasse', 'z. B. VW Golf', 1.52, 1.85, 4.45),
+  Vehicle('class-sedan', 'Limousine / Kombi', 'z. B. BMW 3er', 1.55, 1.90, 4.90),
+  Vehicle('class-suv', 'SUV', 'z. B. VW Tiguan', 1.75, 1.95, 4.75),
+  Vehicle('class-van', 'Van / Transporter', 'z. B. Mercedes Vito', 2.05, 2.05, 5.35),
+];
