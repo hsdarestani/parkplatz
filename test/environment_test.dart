57b2c405cbe23339513adf70c54c8beaabf42080
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-26 22:15:36 +0330

    test: prevent relative API URLs in native releases

---
+++ test/environment_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:parking/config/environment.dart';
+
+void main() {
+  test('default API base URL is safe for native release builds', () {
+    final uri = Uri.parse(Environment.apiBaseUrl);
+
+    expect(uri.isAbsolute, isTrue);
+    expect(uri.scheme, 'https');
+    expect(uri.host, 'parkplatz.smarbiz.sbs');
+    expect(uri.path, '/api');
+  });
+}
